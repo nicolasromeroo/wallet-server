041558270860eda2b53d1d169ffcfa27fcf6447e
nicolasromeroo <nicolasdev290300@gmail.com>
2026-05-11 01:18:59 -0300

    fix: too many connections

--- src/gasto/gasto.module.ts
+++ src/gasto/gasto.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { GastoService } from './gasto.service';
 import { GastoController } from './gasto.controller';
-import { PrismaService } from 'src/prisma/prisma.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [GastoController],
-  providers: [GastoService, PrismaService],
+  providers: [GastoService],
 })
 export class GastoModule {}
